965e66660bf2e73992b53b0c5c5a8706249a8775
KomalKumari17 <komalydv2760@gmail.com>
2025-04-15 14:02:02 +0530

    Add initial project structure with requirements and .gitignore

--- studypoint/cms/tests.py
+++ studypoint/cms/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from .models import *
 
 # Create your tests here.
+# model test case
+
+class FirstTestCase(TestCase):
+    def setUp(self):
+        print("Setting up the test case...")
+
+    def test_topic(self):
+        topics = ["Math", "Science", "History"]
+        for topic in topics:
+            obj = Topics.objects.create(
+                name=topic,
+                description=f"This is a description for {topic}."
+            )
+            self.assertEqual(topic, obj.name)
+
+        objs = Topics.objects.all()
+        self.assertEqual(objs.count(), 3)
